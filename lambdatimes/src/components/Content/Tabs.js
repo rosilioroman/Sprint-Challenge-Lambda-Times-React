6e86ead29bd71f961b9fb7332cb0e3f7552adbf7
rosilioroman <rosilioroman@gmail.com>
2019-02-22 09:55:24 -0700

    Update componentDidMount() on Content.js. Pass down the tabs as a prop to Tabs.js. Type check that prop on Tabs.js

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Tab from './Tab';
 const Tabs = props => {
   return (
@@ -13,4 +14,8 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
 export default Tabs;
